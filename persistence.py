afcb3f4111403d239002006b600a07ab23b9d48b
Sleepy026 <mrbrowngarden@gmail.com>
2019-07-03 11:37:19 +0200

    showboards and DOM in progress

--- persistence.py
+++ persistence.py
@@ -1,54 +1,7 @@
-import csv
 import database_connection
 from psycopg2 import sql
 
-STATUSES_FILE = './data/statuses.csv'
-BOARDS_FILE = './data/boards.csv'
-CARDS_FILE = './data/cards.csv'
-
 _cache = {}  # We store cached data in this dict to avoid multiple file readings
-
-
-@database_connection.connection_handler
-def get_data_from_table(cursor, table_name):
-    sql_query = """
-                SELECT * FROM {table_name};
-                """
-
-    sql_query = sql.SQL(sql_query).format(table_name=sql.Identifier(table_name))
-
-    cursor.execute(sql_query, {'table_name': table_name})
-    result = cursor.fetchall()
-
-    return result
-
-
-def _read_csv(file_name):
-    """
-    Reads content of a .csv file
-    :param file_name: relative path to data file
-    :return: OrderedDict
-    """
-    with open(file_name) as boards:
-        rows = csv.DictReader(boards, delimiter=',', quotechar='"')
-        formatted_data = []
-        for row in rows:
-            formatted_data.append(dict(row))
-        return formatted_data
-
-
-def _get_data(data_type, file, force):
-    """
-    Reads defined type of data from file or cache
-    :param data_type: key where the data is stored in cache
-    :param file: relative path to data file
-    :param force: if set to True, cache will be ignored
-    :return: OrderedDict
-    """
-    if force or data_type not in _cache:
-        _cache[data_type] = _read_csv(file)
-    return _cache[data_type]
-
 
 def clear_cache():
     for key in list(_cache.keys()):
@@ -65,3 +18,17 @@ def get_boards(force=False):
 
 def get_cards(force=False):
     return _get_data('cards', CARDS_FILE, force)
+
+
+@database_connection.connection_handler
+def get_data_from_table(cursor, table_name):
+    sql_query = """
+                SELECT * FROM {table_name};
+                """
+
+    sql_query = sql.SQL(sql_query).format(table_name=sql.Identifier(table_name))
+
+    cursor.execute(sql_query, {'table_name': table_name})
+    result = cursor.fetchall()
+
+    return result
