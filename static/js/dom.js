eeefd9e79fec16914395f20ca2108615dec8e0aa
zoltanMaroti <zoli.maroti@gmail.com>
2019-07-03 14:47:32 +0200

    iterating cards

--- static/js/dom.js
+++ static/js/dom.js
@@ -2,57 +2,66 @@
 import {dataHandler} from "./data_handler.js";
 
 export let dom = {
-        _appendToElement: function (elementToExtend, textToAppend, prepend = false) {
-            // function to append new DOM elements (represented by a string) to an existing DOM element
-            let fakeDiv = document.createElement('div');
-            fakeDiv.innerHTML = textToAppend.trim();
+    _appendToElement: function (elementToExtend, textToAppend, prepend = false) {
+        // function to append new DOM elements (represented by a string) to an existing DOM element
+        let fakeDiv = document.createElement('div');
+        fakeDiv.innerHTML = textToAppend.trim();
 
-            for (let childNode of fakeDiv.childNodes) {
-                if (prepend) {
-                    elementToExtend.prependChild(childNode);
-                } else {
-                    elementToExtend.appendChild(childNode);
-                }
+        for (let childNode of fakeDiv.childNodes) {
+            if (prepend) {
+                elementToExtend.prependChild(childNode);
+            } else {
+                elementToExtend.appendChild(childNode);
             }
+        }
 
-            return elementToExtend.lastChild;
-        },
-        init: function () {
-            // This function should run once, when the page is loaded.
-        },
-        loadBoards: function () {
-            // retrieves boards and makes showBoards called
-            dataHandler.getBoards(function (boards) {
-                dom.showBoards(boards);
+        return elementToExtend.lastChild;
+    },
+    init: function () {
+        // This function should run once, when the page is loaded.
+    },
+    loadBoards: function () {
+        // retrieves boards and makes showBoards called
+        dataHandler.getBoards(function (boards) {
+            dom.showBoards(boards);
 
-            });
-        },
-        loadCards: function () {
-            // retrieves boards and makes showBoards called
-            dataHandler.getAllCards(function (cards) {
-                dom.showCards(cards);
-            });
-        },
-        showBoards: function (boards) {
-            // it adds necessary event listeners also
+        });
+    },
+    loadCards: function () {
+        // retrieves boards and makes showBoards called
+        dataHandler.getAllCards(function (cards) {
+            dom.showCards(cards);
+        });
+    },
+    showBoards: function (boards) {
+        // it adds necessary event listeners also
 
-            // shows boards appending them to #boards div
-            dom.loadCards();
-            const boardsTemplate = document.querySelector('#board-template');
-            const clone = document.importNode(boardsTemplate.content, true);
-            clone.querySelector('.board-title').textContent = boards[0].title;
-            document.querySelector('.board-container').appendChild(clone);
+        // shows boards appending them to #boards div
+        dom.loadCards();
+        const boardsTemplate = document.querySelector('#board-template');
+        const clone = document.importNode(boardsTemplate.content, true);
+        clone.querySelector('.board-title').textContent = boards[0].title;
+        document.querySelector('.board-container').appendChild(clone);
 
 
-        },
-        showCards: function (cards) {
-            // shows the cards of a board
-            // it adds necessary event listeners also
-            const cardTemplate = document.querySelector('#card-template');
+    },
+    showCards: function (cards) {
+        // shows the cards of a board
+        // it adds necessary event listeners also
+        const cardTemplate = document.querySelector('#card-template');
+        for (let card of cards) {
             const clone = document.importNode(cardTemplate.content, true);
-            clone.querySelector('.card-title').textContent = cards[0].title;
-            document.querySelector('.done').appendChild(clone);
-
-        },
+            clone.querySelector('.card-title').textContent = card.title;
+            if (card.status_id === 1) {
+                document.querySelector('.new').appendChild(clone);
+            } else if (card.status_id === 2) {
+                document.querySelector('.in-progress').appendChild(clone);
+            } else if (card.status_id === 3) {
+                document.querySelector('.testing').appendChild(clone);
+            } else if (card.status_id === 4) {
+                document.querySelector('.done').appendChild(clone);
+            }
+        }
+    }
+};
 // here comes more features
-    };
